fe101a68f7594315e5e6b92931066c190dd3e710
Akidoo <plunkels@gmail.com>
2023-04-03 12:11:55 +0800

    First commit

---
+++ index.js
@@ -0,0 +1,19 @@
+console.log('Hello World');
+let name = "Akaash";
+console.log(name);
+
+const interestRate = 0.3;
+console.log(interestRate);
+//use const for variables that do not need to be reassigned
+
+window.alert("bigchungus");
+
+document.getElementById("p1").innerHTML = "Hello " + name;
+
+let username;
+
+document.getElementById("myButton").onclick = function(){
+
+    username = document.getElementById("myText").value;
+    console.log(username);
+}
